9f0056e6a8b69b91b8d79245ca3278d1118dea88
Jaideep Kekre <jaideepkekre@capiot.com>
2018-04-03 12:00:13 +0530

    removed stray printlns

--- goabber.go
+++ goabber.go
@@ -4,7 +4,6 @@
 package goabber
 
 import (
-	"fmt"
 	"unicode/utf8"
 )
 
@@ -17,24 +16,22 @@ func Abber(inputAbber string, target string) bool {
 
 func isAbber(inputAbber string, target string, returnvalue bool) bool {
 
-	fmt.Println("abber: ", utf8.RuneCountInString(inputAbber))
-	fmt.Println("target:" + target)
 	if utf8.RuneCountInString(inputAbber) == 0 || returnvalue == true {
 		returnvalue = true
 
 	} else if utf8.RuneCountInString(inputAbber) > 0 && utf8.RuneCountInString(target) == 0 {
 		returnvalue = false
 	} else if inputAbber[0] == target[0] {
-		fmt.Println("Matched : " + string(inputAbber[0]))
+
 		inputAbber = inputAbber[1:]
 		target = target[1:]
 		returnvalue = isAbber(inputAbber, target, returnvalue)
 
 	} else {
-		fmt.Println("skipping : " + string(target[0]))
+
 		target = target[1:]
 		returnvalue = isAbber(inputAbber, target, returnvalue)
 	}
-	fmt.Println(returnvalue)
+
 	return returnvalue
 }
